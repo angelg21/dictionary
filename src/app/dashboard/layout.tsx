f32e1d49b011f7a5a18c69a73370bdc262b6b4f1
Angel Guevara <guevarangel14@gmail.com>
2024-08-16 12:52:40 -0400

    Fixed sidebar status on mobile

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,25 @@
+'use client'
+
 import { Profile } from "@/src/dashboard/components/Profile/Profile";
 import { Sidebar } from "@/src/dashboard/components/Sidebar/Sidebar";
+import { useState } from "react";
 
 
 export default function DashboardLayout({ children }: { children: React.ReactNode; }) {
+
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+
+    const handleSidebar = (status: boolean) => {
+        setSidebarOpen(status);
+    };
+
     return (
         <div className="bg-d-fondo antialiased">
 
             <div className="flex ">
-                <Sidebar/>
+                <Sidebar sendStatusSidebar={handleSidebar} statusSidebar={sidebarOpen}/>
                 <div className="min-h-screen flex flex-col">
-                    <Profile/>
+                    <Profile sendStatusSidebar={handleSidebar}/>
                     <div className="flex-1 lg:ml-[256px]">
 
                     { children }
