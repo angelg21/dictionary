534f8036a7d3cbb89937f96ac561a348b94d3b1e
Angel Guevara <guevarangel14@gmail.com>
2024-08-28 20:53:57 -0400

    fixed layout issues

--- src/app/dashboard/worksheets/layout.tsx
+++ src/app/dashboard/worksheets/layout.tsx
@@ -7,71 +7,77 @@ import { TabsMenu } from "@/src/worksheets/componentes/TabsMenu/TabsMenu";
 import { MagnifyingGlassIcon, PlusIcon } from '@heroicons/react/24/outline'
 import { useState } from "react";
 import { NewWorkSheetModal } from "@/src/worksheets/componentes/NewWorkSheetModal/NewWorkSheetModal";
-
-
-
+import { useWorksheetsContext, WorksheetsProvider } from "@/src/worksheets/context/WorkSheetsContext";
 
 export default function WorksheetsLayout({ children }: { children: React.ReactNode; }) {
-    const [isNewWorkSheetOpen, setNewWorkSheetOpen] = useState(false);
+  return (
+    <WorksheetsProvider>
+      <LayoutContent>
+        {children}
+      </LayoutContent>
+    </WorksheetsProvider>
+  );
+}
 
-    const handleCloseNewWorkSheetModal = () => {
-        setNewWorkSheetOpen(false);
-    };
+function LayoutContent({ children }: { children: React.ReactNode; }) {
 
-    const handleOpenNewWorkSheetModal = () => {
-        setNewWorkSheetOpen(true);
-    };
+  const [isNewWorkSheetOpen, setNewWorkSheetOpen] = useState(false);
+  const handleCloseNewWorkSheetModal = () => {
+    setNewWorkSheetOpen(false);
+  };
+  const handleOpenNewWorkSheetModal = () => {
+    setNewWorkSheetOpen(true);
+  };
+  const { setSearchTerm, setFilterType } = useWorksheetsContext();
 
-
-    return (
+  return (
         <div>
-            <div className="flex flex-col mx-5 lg:mx-16 xl:mx-20">
-                <div className="flex justify-between mt-2">
-                    <span className="text-4xl text-d-blue font-bold ">Mis Fichas</span>
-                    <div className="p-2 rounded-full bg-d-green xl:hidden ">
-                        <PlusIcon className="h-6 w-6 text-white" />
-                    </div>
-                </div>
-                <div className="flex flex-col sm:flex-row mt-5 justify-between">
-                    <TabsMenu />
-                    <div onClick={() => handleOpenNewWorkSheetModal()}>
+          <div className="flex flex-col mx-5 lg:mx-16 xl:mx-20">
+            <div className="flex justify-between mt-2">
+              <span className="text-4xl text-d-blue font-bold ">Mis Fichas</span>
+              <div className="p-2 rounded-full bg-d-green xl:hidden ">
+                <PlusIcon className="h-6 w-6 text-white" />
+              </div>
+            </div>
+            <div className="flex flex-col sm:flex-row mt-5 justify-between">
+              <TabsMenu />
+              <div onClick={() => handleOpenNewWorkSheetModal()}>
 
-                        <ButtonWithIconLeft
-                            title="Nueva Ficha"
-                            textColor="text-white"
-                            backgroundColor="bg-d-green hidden xl:flex"
-                            hover="hover:bg-d-green-light"
-                            icon=<PlusIcon />
-                            iconColor="text-white"
-                        />
-                    </div>
-                </div>
-
-                <div className="flex flex-col xl:flex-row xl:items-center my-5 justify-between ">
-                    <InputWithIconLeft
-                        name="searchWorkSheets"
-                        type="text" icon=<MagnifyingGlassIcon />
-                        iconColor="text-d-gray-light"
-                        inputWidth="w-full max-w-lg lg:w-[280px]"
-                        hover="hover:ring-gray-400"
-                    />
-                    <div className="mt-5 xl:mt-0">
-                        <Filters />
-                    </div>
-                </div>
-                {children}
+                <ButtonWithIconLeft
+                  title="Nueva Ficha"
+                  textColor="text-white"
+                  backgroundColor="bg-d-green hidden xl:flex"
+                  hover="hover:bg-d-green-light"
+                  icon=<PlusIcon />
+                  iconColor="text-white"
+                />
+              </div>
             </div>
 
-            {isNewWorkSheetOpen && (
-                <NewWorkSheetModal
-
-                    onClose={handleCloseNewWorkSheetModal}
+            <div className="flex flex-col xl:flex-row xl:items-center my-5 justify-between ">
+              <InputWithIconLeft
+                name="searchWorkSheets"
+                type="text" icon=<MagnifyingGlassIcon />
+                iconColor="text-d-gray-light"
+                inputWidth="w-full max-w-lg lg:w-[280px]"
+                hover="hover:ring-gray-400"
+                onSearchChange={setSearchTerm}
+              />
+              <div className="mt-5 xl:mt-0">
+                <Filters
+                  onFilterChange={setFilterType}
                 />
-            )}
+              </div>
+            </div>
+            {children}
+          </div>
 
+          {isNewWorkSheetOpen && (
+            <NewWorkSheetModal
+
+              onClose={handleCloseNewWorkSheetModal}
+            />
+          )}
         </div>
-      </div>
-      {children}
-    </div>
   );
 }
