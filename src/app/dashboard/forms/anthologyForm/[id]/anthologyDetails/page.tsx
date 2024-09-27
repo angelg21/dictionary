aed82c3589584ac5c4db16fedc2a8e5b0d3151a0
Angel Guevara <guevarangel14@gmail.com>
2024-09-27 09:43:16 -0400

    Add get-forms

--- src/app/dashboard/forms/anthologyForm/[id]/anthologyDetails/page.tsx
+++ src/app/dashboard/forms/anthologyForm/[id]/anthologyDetails/page.tsx
@@ -1,7 +1,7 @@
 import { AnthologyDetailsForm } from "@/src/forms/components/AnthologyFormComponents/AnthologyDetailsForm/AnthologyDetailsForm";
 import { AnthologyProgressBar } from "@/src/forms/components/AnthologyFormComponents/AnthologyProgressBar/AnthologyProgressBar";
 
-
+export const revalidate = 0;
 
 export default function AnthologyDetails() {
     return (
